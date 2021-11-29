205cd36a640566ed47379718f908bc17cf0264bb
tmassart873 <tim.massart@hotmail.com>
2021-11-29 16:05:07 +0100

    build (BookMapper class) : created BookMapper class

--- src/main/java/com/lolweb/digibooky/service/mappers/BookMapper.java
+++ src/main/java/com/lolweb/digibooky/service/mappers/BookMapper.java
@@ -1,4 +1,31 @@
 package com.lolweb.digibooky.service.mappers;
 
+import com.lolweb.digibooky.domain.book.Book;
+import com.lolweb.digibooky.service.dtos.BookDto;
+import com.lolweb.digibooky.service.dtos.CreateBookDto;
+import org.springframework.stereotype.Component;
+
+import static com.lolweb.digibooky.domain.book.Book.*;
+
+@Component
 public class BookMapper {
+
+    public BookDto mapToBookDto(Book book) {
+        return new BookDto()
+                .setId(book.getId())
+                .setIsbn(book.getIsbn())
+                .setTitle(book.getTitle())
+                .setAuthor(book.getAuthor())
+                .setAvailable(book.isAvailable());
+    }
+
+    public Book mapToBook(CreateBookDto createBookDto) {
+        return BookBuilder
+                .bookBuilder()
+                .withAuthor(createBookDto.getAuthor())
+                .withIsbn(createBookDto.getIsbn())
+                .withTitle(createBookDto.getTitle())
+                .withIsAvailable(createBookDto.isAvailable())
+                .build();
+    }
 }
