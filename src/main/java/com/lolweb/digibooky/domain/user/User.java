a88123c065f5cfee4f2a420a4b01877fe8caf982
rudykierbel <rudykierbel@gmail.com>
2021-11-29 14:27:43 +0100

    Build (user class): updated user class

--- src/main/java/com/lolweb/digibooky/domain/user/User.java
+++ src/main/java/com/lolweb/digibooky/domain/user/User.java
@@ -1,4 +1,89 @@
 package com.lolweb.digibooky.domain.user;
 
+import com.lolweb.digibooky.domain.address.Address;
+import com.lolweb.digibooky.domain.emailaddress.EmailAddress;
+
+import java.util.UUID;
+
 public class User {
+
+    private final UUID id;
+    private final String firstName;
+    private final String lastName;
+    private final EmailAddress emailAddress;
+    private final String inss;
+    private final Role role;
+    private final Address address;
+
+    public User(UserBuilder builder) {
+        id = builder.id;
+        firstName = builder.firstName;
+        lastName = builder.lastName;
+        emailAddress = builder.emailAddress;
+        inss = builder.inss;
+        role = builder.role;
+        address = builder.address;
+    }
+
+    private enum Role {
+        ADMIN,
+        LIBRARIAN,
+        MEMBER
+    }
+
+    public static final class UserBuilder {
+        private UUID id;
+        private String firstName;
+        private String lastName;
+        private EmailAddress emailAddress;
+        private String inss;
+        private Role role;
+        private Address address;
+
+        private UserBuilder() {
+        }
+
+        public static UserBuilder userBuilder() {
+            return new UserBuilder();
+        }
+
+        public User build() {
+            return new User(this);
+        }
+
+        public UserBuilder withId(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder withFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public UserBuilder withLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public UserBuilder withEmailAddress(EmailAddress emailAddress) {
+            this.emailAddress = emailAddress;
+            return this;
+        }
+
+        public UserBuilder withInss(String inss) {
+            this.inss = inss;
+            return this;
+        }
+
+        public UserBuilder withRole(Role role) {
+            this.role = role;
+            return this;
+        }
+
+        public UserBuilder withAddress(Address address) {
+            this.address = address;
+            return this;
+        }
+    }
 }
