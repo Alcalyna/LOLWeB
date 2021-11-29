4a7577062188799be6f34119957a493a0aee0442
rudykierbel <rudykierbel@gmail.com>
2021-11-29 13:50:18 +0100

    Build (Book class): updated book class

--- src/main/java/com/lolweb/digibooky/domain/book/Book.java
+++ src/main/java/com/lolweb/digibooky/domain/book/Book.java
@@ -1,5 +1,42 @@
 package com.lolweb.digibooky.domain.book;
 
+import com.lolweb.digibooky.domain.author.Author;
+
+import java.util.UUID;
+
 public class Book {
 
+    private final UUID id;
+    private final String isbn;
+    private final String title;
+    private final Author author;
+    private boolean isAvailable;
+
+    public Book(String isbn, String title, Author author, boolean isAvailable) {
+        this.id = UUID.randomUUID();
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = isAvailable;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
 }
