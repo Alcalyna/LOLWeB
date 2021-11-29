06f2f15934612146eb929ed02d7ab8153f5f98be
rudykierbel <rudykierbel@gmail.com>
2021-11-29 14:16:59 +0100

    Build (bookloan class): updated bookloan class

--- src/main/java/com/lolweb/digibooky/domain/loan/BookLoan.java
+++ src/main/java/com/lolweb/digibooky/domain/loan/BookLoan.java
@@ -1,4 +1,35 @@
 package com.lolweb.digibooky.domain.loan;
 
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class BookLoan {
+
+    private final UUID id;
+    private LocalDate dueDate;
+    private final UUID borrowerId;
+    private final String borrowedBookIsbn;
+
+    public BookLoan(LocalDate dueDate, UUID borrowerId, String borrowedBookIsbn) {
+        this.id = UUID.randomUUID();
+        this.dueDate = dueDate;
+        this.borrowerId = borrowerId;
+        this.borrowedBookIsbn = borrowedBookIsbn;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public UUID getBorrowerId() {
+        return borrowerId;
+    }
+
+    public String getBorrowedBookIsbn() {
+        return borrowedBookIsbn;
+    }
 }
