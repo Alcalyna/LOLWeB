d059e2a1a550852a35b1ada9eea33f4e9c865c25
Linh P. Nguyen <nguyenlp.info@gmail.com>
2021-11-29 15:24:06 +0100

    build(BookLoan): testing getters

---
+++ src/test/java/com/lolweb/digibooky/domain/loan/BookLoanTest.java
@@ -0,0 +1,39 @@
+package com.lolweb.digibooky.domain.loan;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookLoanTest {
+    BookLoan bookLoan;
+
+    @BeforeEach
+    public void init() {
+        bookLoan = new BookLoan(LocalDate.now(), UUID.randomUUID(), "12345678900000");
+    }
+
+    @Test
+    void getId() {
+        Assertions.assertEquals(UUID.class, bookLoan.getId().getClass());
+    }
+
+    @Test
+    void getDueDate() {
+        Assertions.assertEquals(LocalDate.now(), bookLoan.getDueDate());
+    }
+
+    @Test
+    void getBorrowerId() {
+        Assertions.assertEquals(UUID.class, bookLoan.getBorrowerId().getClass());
+    }
+
+    @Test
+    void getBorrowedBookIsbn() {
+        Assertions.assertEquals("12345678900000", bookLoan.getBorrowedBookIsbn());
+    }
+}
