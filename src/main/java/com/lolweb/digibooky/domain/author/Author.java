0d06ff5610f47c9e1a38b3cca08c3e93257d9936
rudykierbel <rudykierbel@gmail.com>
2021-11-29 13:47:12 +0100

    Build (Author class): completed author class

--- src/main/java/com/lolweb/digibooky/domain/author/Author.java
+++ src/main/java/com/lolweb/digibooky/domain/author/Author.java
@@ -1,4 +1,28 @@
 package com.lolweb.digibooky.domain.author;
 
+import java.util.UUID;
+
 public class Author {
+
+    private final UUID id;
+    private final String firstName;
+    private final String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.id = UUID.randomUUID();
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
