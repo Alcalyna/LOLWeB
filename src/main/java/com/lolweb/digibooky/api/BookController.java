31335085c141954ef36c320c5e2c096f4da1d1b7
tmassart873 <tim.massart@hotmail.com>
2021-11-29 16:03:36 +0100

    build (BookController class) : created BookController + implementation of feature getAllBooksInLibrary

--- src/main/java/com/lolweb/digibooky/api/BookController.java
+++ src/main/java/com/lolweb/digibooky/api/BookController.java
@@ -1,4 +1,27 @@
 package com.lolweb.digibooky.api;
 
+import com.lolweb.digibooky.service.BookService;
+import com.lolweb.digibooky.service.dtos.BookDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+
+    private BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+    @GetMapping(produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public List<BookDto> getAllBooksInLibrary(){
+        return bookService.getAllBooksInLibrary();
+    }
 }
