206026e0554fc06cc9f9f18c8203f789ef695a07
tmassart873 <tim.massart@hotmail.com>
2021-11-29 16:28:14 +0100

    build (BookController class) : added the getBookById

--- src/main/java/com/lolweb/digibooky/api/BookController.java
+++ src/main/java/com/lolweb/digibooky/api/BookController.java
@@ -1,14 +1,13 @@
 package com.lolweb.digibooky.api;
 
+import com.lolweb.digibooky.domain.book.Book;
 import com.lolweb.digibooky.service.BookService;
 import com.lolweb.digibooky.service.dtos.BookDto;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/books")
@@ -23,5 +22,12 @@ public class BookController {
     @ResponseStatus(HttpStatus.OK)
     public List<BookDto> getAllBooksInLibrary(){
         return bookService.getAllBooksInLibrary();
+    }
+
+    @GetMapping(produces = "application/json", path = "/id")
+    @ResponseStatus(HttpStatus.OK)
+    public BookDto getBookById (@PathVariable ("id")UUID id){
+        return bookService.getBookById(id);
+
     }
 }
