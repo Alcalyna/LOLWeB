60451106e9a646899f3771dcc6f98f3ef32e2aa9
tmassart873 <tim.massart@hotmail.com>
2021-11-29 16:06:22 +0100

    build (BookRepository class) : created BookRepository class

---
+++ src/main/java/com/lolweb/digibooky/domain/BookRepository.java
@@ -0,0 +1,28 @@
+package com.lolweb.digibooky.domain;
+
+import com.lolweb.digibooky.domain.book.Book;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class BookRepository {
+    private HashMap<UUID, Book> booksInLibrary;
+
+    public BookRepository() {
+        this.booksInLibrary = new HashMap<>();
+    }
+
+    public Book save(Book book){
+        booksInLibrary.put(book.getId(),book);
+        return book;
+    }
+
+    public List<Book> getAll(){
+        return booksInLibrary.values().stream().collect(Collectors.toList());
+    }
+
+}
