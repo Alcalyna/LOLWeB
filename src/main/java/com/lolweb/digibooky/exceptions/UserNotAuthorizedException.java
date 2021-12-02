f3183e2a21c641f05c11b99c4091e3b6031f6dbd
Lucia Madero <maderolucia@gmail.com>
2021-12-02 14:53:23 +0100

    test (Story 9: Register Librarian) : test 403 response

--- src/main/java/com/lolweb/digibooky/exceptions/UserNotAuthorizedException.java
+++ src/main/java/com/lolweb/digibooky/exceptions/UserNotAuthorizedException.java
@@ -3,6 +3,7 @@ package com.lolweb.digibooky.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class UserNotAuthorizedException extends RuntimeException {
 
     public UserNotAuthorizedException(String message) {
